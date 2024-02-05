c509b6d3c2a5196cefaa940d3476de4323fefe85
Steven <steven@pm.me>
2024-02-05 19:57:15 +1100

    setup logging

--- src/views/ask-axel/hooks/use-axel-chats-fixed.vitest.tsx
+++ src/views/ask-axel/hooks/use-axel-chats-fixed.vitest.tsx
@@ -1,18 +1,28 @@
 import { renderHook, waitFor } from '@testing-library/react';
 
 import { useAxelChats } from './use-axel-chats';
-import { mswServer } from '../../../api/msw-server';
 import { ApolloWrapper } from '../../../api/apollo-wrapper';
 import { listChatsResponseHandler } from '../../../api/axel/mocks/handlers';
 import { CHATS } from '../../../api/axel/mocks/data';
-import { setupServer } from 'msw/lib/node';
+import { setupServer } from 'msw/node';
 
 const wrapper = ApolloWrapper;
 
 const server = setupServer(listChatsResponseHandler);
+server.events.on('request:start', async ({ request }) => {
+  // const payload = await request.clone().json();
+  console.log('Start process for handler with operationName', request);
+});
 
 describe('useAxelChats', () => {
-  beforeAll(() => server.listen());
+  beforeAll(() =>
+    server.listen({
+      onUnhandledRequest: async (request) => {
+        const payload = await request.clone().json();
+        console.log('No handler for operationName', payload.operationName);
+      },
+    })
+  );
   afterAll(() => server.close());
   afterEach(() => server.resetHandlers());
 
