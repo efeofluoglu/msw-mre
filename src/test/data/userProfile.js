a31f0d8278985dde1fb192fba1ac267fd3b2f614
Steven <steven@pm.me>
2024-02-05 19:23:49 +1100

    add userProfile to fix import issues

---
+++ src/test/data/userProfile.js
@@ -0,0 +1,42 @@
+const profile = {
+  pk: 'max.mustermann@bp.com',
+  profile: {
+    expertise_areas: [],
+    skill_areas: [],
+    contact_details: [
+      {
+        name: 'Email',
+        is_visible: true,
+        icon_url:
+          'http://localhost:8080/media/documents/email-contact-medium_g0KVOMH.svg',
+        link: 'mailto:max.mustermann@bp.com',
+      },
+      {
+        name: 'Teams',
+        is_visible: true,
+        icon_url:
+          'http://localhost:8080/media/documents/teams-contact-medium_Ofc8TBn.svg',
+        link: 'msteams:/l/chat/0/0?users=max.mustermann@bp.com&deeplinkId=d9ecf6d6-082c-4edf-92fd-06ff1cd89876&launchAgent=join_launcher&type=chat&directDl=true&msLaunch=true&enableMobilePage=true&suppressPrompt=true&fqdn=teams.microsoft.com',
+      },
+      {
+        name: 'Slack',
+        is_visible: true,
+        icon_url:
+          'http://localhost:8080/media/documents/slack-contact-medium_Fq5p2VV.svg',
+        link: 'slack://user?team=T0220L0L9NU&id=U02D1QH0SN7',
+      },
+    ],
+    ado_license_requested: false,
+    id: 100,
+    job_title: 'Front End / UI developer',
+    photo_url: '/media/documents/avatar1_qfbeKKH.svg',
+    user: 'max.mustermann@bp.com',
+  },
+  email: 'max.mustermann@bp.com',
+  first_name: 'Max',
+  last_name: 'Mustermann',
+  full_name: 'Max Mustermann',
+  last_login: '2022-03-16T06:08:10.318475Z',
+};
+
+export default profile;
